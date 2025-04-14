2f2f152edb7c156af87b6e8bc26cbf415200e144
Berihuete Nunez, Engel <mastering23@gmail.com>
2025-04-14 19:31:22 -0400

    adding GUI GREETING + Name  practices

--- appGui.py
+++ appGui.py
@@ -1,1 +1,20 @@
-print("TESTING")
+import tkinter
+window = tkinter.Tk()
+window.geometry("400x400")
+name_var = tkinter.StringVar()
+
+
+def change_name():
+  greeting_name.configure(text="Hello " + name_var.get())
+
+
+greeting_name = tkinter.Label(window,text="Hello",font=("Arial",18),padx=20,pady=10)
+greeting_name.pack()
+
+name = tkinter.Entry(window, font=("Arial",16),textvariable=name_var)
+name.pack(padx=20,ipady=10)
+
+change_name_btn = tkinter.Button(window,text="Change Name",font=("Arial",16),command=change_name)
+change_name_btn.pack(padx=20,pady=10)
+
+window.mainloop()
